f3cbff45475446d28400c00c25cd248c1d9fa3f6
Kevin Clement <kevin.clement@gmail.com>
2018-11-11 21:17:56 -0800

    basic bluetooth and serial handling

--- arduino-painting.ino
+++ arduino-painting.ino
@@ -1,27 +1,65 @@
-int sense = A6;        // sensing LED connected to analog5
+#include "BluetoothSerial.h"
+
+int sense = A6;       // sensing LED connected to analog5
 int val   = 0;        // variable to store the value read from sense
+
+BluetoothSerial SerialBT;
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("lightsense");
-  Serial.println(sense);
+  Serial.println("Painting bottom dropper by kevinc");
+  SerialBT.begin("ExitPaint"); //Bluetooth device name
 
   pinMode(14, OUTPUT); 
-  // TODO: arguments to allow for dumping of sensing values
+}
+
+
+void readAnyBluetoothMessage() { 
+  if (!SerialBT.available()) {
+    return;
+  }
+
+  String str = SerialBT.readStringUntil('\n');
+
+  if (str == "enable") {
+    Serial.println("got enabled command");
+  }
+  else if (str == "disable") {
+    Serial.println("got disabled command");
+  } else {
+    Serial.print("unknown command: ");
+    Serial.println(str);
+  }
+}
+
+void readAnySerialMessage() {
+  if (!Serial.available()) {
+    return;
+  }
+
+  String str = Serial.readStringUntil('\n');
+  Serial.print("Serial command: ");
+  Serial.println(str);
+
+  // send it to bluetooth device
+  SerialBT.print(str);
 }
 
 void loop() {
-  // Serial.printf("ESP32 Chip ID = %04X",(uint16_t)(chipid>>32));//print High 2 bytes
+  // read bluetooth messages
+  readAnyBluetoothMessage();
 
-  val = analogRead(sense);
-  Serial.println(val);
-  if (val == 0) {
-    digitalWrite(14, LOW);   
-  } else {
-    digitalWrite(14, HIGH);   
-    
-  }
+  // read serial messages
+  readAnySerialMessage();
+
+//  val = analogRead(sense);
+//  Serial.println(val);
+//  if (val == 0) {
+//    digitalWrite(14, LOW);   
+//  } else {
+//    digitalWrite(14, HIGH);   
+//    
+//  }
   
-
   delay(50);
 }
