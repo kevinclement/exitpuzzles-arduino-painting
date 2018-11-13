3126a787d0dbe54f594a57c1c91f80d0d155c991
Kevin Clement <kevin.clement@gmail.com>
2018-11-12 22:28:16 -0800

    print functionality for single value

--- arduino-painting.ino
+++ arduino-painting.ino
@@ -5,22 +5,22 @@ int  MAGNET_PIN = 14;                 // pin that controls the magnet
 int  LIGHT_THRESHOLD = 0;             // what threshold to hit before we trigger drop
 int  LIGHT_THRESHOLD_WAIT_MS = 1500;  // how long to wait at threshold before we trigger the drop, in milliseconds
 bool REALTIME_ENABLED = false;
+bool PRINT_ENABLED = false;
 int  DROP_OVERRIDE = 0;
 int  DROP_OVERRIDE_TIMEOUT = 2000;
 
 unsigned long dark_detected_timestamp = 0;                   // when we started to detect dark
 unsigned long drop_override_timestamp = 0;  
-int DROP = LOW;
-bool dropped = false;
 
 BluetoothSerial SerialBT;
 
 // TODO: 
 //   [ ] commands
+//     [ ] enable - enables light detection to work
 //     [x] drop - drops key
 //     [ ] threshold n - sets threshold to go below to trigger
 //     [ ] wait n - sets how long to wait below threshold before triggering
-//     [ ] print - print current reading from sensor
+//     [x] print - print current reading from sensor
 //     [x] realtime - print current reading out on console in realtime
 //   [x] implement threshold before dropping
 //   [ ] implement eeprom usage for storing threshold and wait
@@ -37,6 +37,7 @@ void setup() {
 
 void printHelp() {
   Serial.println("Available commands:");
+  Serial.println("  enable      - turns light detection on");
   Serial.println("  drop        - triggers dropping the magnet manually");
   Serial.println("  threshold N - set threshold to be used to detect light");
   Serial.println("  wait N      - set time in milliseconds to wait while at or below threshold before triggering");
@@ -53,6 +54,9 @@ void handleMessage(String msg) {
   }
   else if (msg == "realtime") {
     REALTIME_ENABLED = !REALTIME_ENABLED;
+  }
+  else if (msg == "print") {
+    PRINT_ENABLED = true;
   }
   else if (msg == "drop") {
     DROP_OVERRIDE = 2;
@@ -95,8 +99,13 @@ void loop() {
   // read value coming from light sensor
   int ls = analogRead(LIGHT_SENSOR_PIN);
 
-  if (REALTIME_ENABLED) {
+  if (REALTIME_ENABLED || PRINT_ENABLED) {
     Serial.println(ls);
+  }
+
+  // print enabled is for a single recorded value
+  if (PRINT_ENABLED) {
+    PRINT_ENABLED = false;
   }
 
   // Check of drop override is in place
